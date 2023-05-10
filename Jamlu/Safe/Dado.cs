7cb0082de66891b0a1163f21edc40521c0d02b9f
biklaire <claramontanaro1709@gmail.com>
2023-05-10 21:17:26 +0200

    First steps on Library Approach

--- Jamlu/Safe/Dado.cs
+++ Jamlu/Safe/Dado.cs
@@ -4,20 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jamlu.Safe
+namespace Jamlu
 {
+    /// <summary>
+    /// Classe per il controllo dei tiri dei dadi
+    /// </summary>
+    /// <remarks>
+    /// Questa classe è utilizzata per controllare che un numero generato esternamente sia conforme con il dado specificato ed eventualmente richiederne uno nuovo.
+    /// </remarks>
     public static class Dado
     {
+        /// <summary>
+        /// Controllo del tiro di un dado d4
+        /// </summary>
+        /// <param name="value">
+        /// Il valore da controllare
+        /// </param>
+        /// <returns>
+        /// Restituisce il numero controllato, compreso tra 1 e 4
+        /// </returns>
         public static int D4(string value)
         {
             return value.SafeInt(1, 4);
         }
 
+        /// <summary>
+        /// Controllo del tiro di un dado d6
+        /// </summary>
+        /// <param name="value">
+        /// Il valore da controllare
+        /// </param>
+        /// <returns>
+        /// Restituisce il numero controllato, compreso tra 1 e 6
+        /// </returns>
         public static int D6(string value)
         {
             return value.SafeInt(1, 6);
         }
 
+        /// <summary>
+        /// Controllo del tiro di un dado d20
+        /// </summary>
+        /// <param name="value">
+        /// Il valore da controllare
+        /// </param>
+        /// <returns>
+        /// Restituisce il numero controllato, compreso tra 1 e 20
+        /// </returns>
         public static int D20(string value)
         {
             return value.SafeInt(1, 20);
